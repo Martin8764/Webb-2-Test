40c5ea0bcffcfb0bcfe328bd0ff2cf059a27f5ad
Martin8764 <83948209+Martin8764@users.noreply.github.com>
2021-09-02 14:12:59 +0200

    commit deleted file not found

---
+++ js/Island project script.js
@@ -0,0 +1,8 @@
+// Ställ in sidans navigationsbredd till 0px
+function openNav() {
+    document.getElementById("mySidenav").style.width = "0px";
+}
+// Ställer in sidans navigationsbredd till 316px
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "316px";
+}
